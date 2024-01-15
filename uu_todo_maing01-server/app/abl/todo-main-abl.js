ab7122f0b7f544dc0ec9ad0a11e8b9a8b54d6788
Maros Kovalak <maros.kovalak@plus4u.net>
2024-01-15 14:08:17 +0100

    feat: added createTodo function

--- uu_todo_maing01-server/app/abl/todo-main-abl.js
+++ uu_todo_maing01-server/app/abl/todo-main-abl.js
@@ -147,6 +147,11 @@ class TodoMainAbl {
     // HDS 2
     return dtoOut;
   }
+
+  async createTodo(uri, session, uuAppErrorMap = {}) {
+    // HDS 1
+    return await UuAppWorkspace.loadBasicData(uri, session, uuAppErrorMap);
+  }
 }
 
 module.exports = new TodoMainAbl();
